f7382b5f89ee212ec458fffb83b2939af1755031
Oleg <neperate@gmail.com>
2022-03-26 09:11:46 +0300

    buttom

--- blocks/navigation/navigation.php
+++ blocks/navigation/navigation.php
@@ -1,10 +1,30 @@
 <aside class="navigation">
   <ul class="navigation__list">
     <li class="navigation__list-item">
-      <a class="navigation__link" href="../../index.php">Patterns</a>
+      <button class="navigation__dropdown-button">
+        <i class="fa fa-caret-down"></i>
+        <a class="navigation__link  
+      <?php if ($title == "Patterns") {
+        echo "active";
+      } ?>" href="../../index.php">Patterns</a>
+      </button>
+      <?php
+        if ($title == "Patterns"):
+      ?>
+      <div class="navigation__dropdown-container">
+      
+        <a class="navigation__dropdown-link" href="#">Link1</a>
+        <a class="navigation__dropdown-link" href="#">Link2</a>
+        <a class="navigation__dropdown-link" href="#">Link3</a>
+      </div>
+      <?php endif; ?>
     </li>
+
     <li class="navigation__list-item">
-      <a class="navigation__link" href="../../page/oop.php">OOP</a>
+      <a class="navigation__link  
+      <?php if ($title == "OOP") {
+        echo "active";
+      } ?>" href="../../page/oop.php">OOP</a>
     </li>
   </ul>
 </aside>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <title>
     <?php 
     $title = "Patterns";
-    echo "$title"
+    echo "$title";
     ?>
     </title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -25,6 +25,6 @@
   </main>
 
 
-  
+  <script src="./script/script.js"></script>
 </body>
 </html>
